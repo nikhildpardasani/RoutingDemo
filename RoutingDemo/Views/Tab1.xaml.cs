f125efe22ad973512afe9d246b68b3cf4aa3bff1
Nikhil Pardasani <nikhil_pardasani@yahoo.com>
2019-12-24 14:51:30 +0530

    Added new files

---
+++ RoutingDemo/Views/Tab1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RoutingInShell.Views
+{
+    public partial class Tab1 : ContentPage
+    {
+        public Tab1()
+        {
+            InitializeComponent();
+        }
+
+        void OnButton1Clicked(object sender, EventArgs args)
+        {
+            Shell.Current.GoToAsync("Tab1/Tab1A");
+        }
+
+        void OnButton2Clicked(object sender, EventArgs args)
+        {
+            Shell.Current.GoToAsync("Tab1/Tab1B");
+        }
+
+        void OnButton3Clicked(object sender, EventArgs args)
+        {
+            Shell.Current.GoToAsync("Tab1/Tab1C");
+        }
+    }
+}
